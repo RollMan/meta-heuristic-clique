bd187e31899b918ec2e25e8d80381f3b104bbc9f
Yohei Shimmyo <qwpmb554@gmail.com>
2018-10-30 17:05:02 +0900

    complete main for random case
    todo: 01_hand cases support

--- main.m
+++ main.m
@@ -1,9 +1,23 @@
-nodefile = "testcases/Node-01.in";
-edgefile = "testcases/Edge-01.in";
+NODEFILE = "testcases/02_random_node_%02d.in";
+EDGEFILE = "testcases/02_random_edge_%02d.in";
 
-V = importdata(nodefile)
-E = importdata(edgefile)
-[value, nodes] = SA(V, E)
-% [value, nodes] = TS(V, E)
-% [value, nodes] = ILS(V, E)
-% [value, nodes] = GLS(V, E)
+TESTCASE_NUM = 20;
+for testcase = 1:TESTCASE_NUM
+    V = importdata(sprintf(NODEFILE, testcase-1))
+    E = importdata(sprintf(EDGEFILE, testcase-1));
+    tic;
+    [value, nodes] = SA(V, E)
+    toc;
+    
+    % tic;
+    % [value, nodes] = TS(V, E)
+    % toc;
+    
+    % tic;
+    % [value, nodes] = ILS(V, E)
+    % toc;
+    
+    % tic;
+    % [value, nodes] = GLS(V, E)
+    % toc;
+end
