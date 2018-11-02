803de3ca3de0ad244025ae5a3da738d313403571
Yohei Shimmyo <qwpmb554@gmail.com>
2018-11-02 18:19:39 +0900

    modify evaluation for state structure; existing array

--- evaluation.m
+++ evaluation.m
@@ -5,24 +5,13 @@ function score = evaluation(state, G)
         return;
     end
     
-    for i = 1:size(state, 2)
-        s = state(1, i);
-        if s == 0
-            continue;
-        end
-        for j = 1:size(state, 2)
-            t = state(1, j);
-            if t == 0
-                continue;
-            end
-            if s == t
-                continue;
-            end
-            if ~(G(s,t) == 1 && G(t, s) == 1)
-                score = -1;
-                return
-            end
+    if is_clique(state, G) == false
+        score = -1;
+    else
+        % score = size(state,2);
+        score = 0;
+        for i = 1:size(state,2)
+            score = score + state(i);
         end
     end
-    score = size(state,2);
 end
