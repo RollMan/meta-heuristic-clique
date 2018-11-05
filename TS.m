6a54b4c6e6451f460970e1e2cd28f618d677d3a0
yotsuba382 <m5221130@u-aizu.ac.jp>
2018-11-05 14:55:13 +0900

    [fix] tabu search

--- TS.m
+++ TS.m
@@ -1,24 +1,17 @@
-function [value, nodes] = TS(V, E)
+function [value, nodes] = TS(G,V, E)
 
     N = 10; %探索回数
-    tabu_size = 5; %タブリストに入れておく回数
+    tabu_size = 2; %タブリストに入れておく回数
     nodes = [];
-    nodes_old = [];
     
     
     tabu_list = zeros(1,V);
     now_node  = randi([0 1],1,V); %initialize node
-
-    G = graph;
-    G = addnode(G,V);
-    for node_num = 1:size(E,1)
-        G = addedge(G,E(node_num,1),E(node_num,2));
-    end
-   
-    %Gの接続行列
-    A = adjacency(G);
+    disp(now_node);
     
-    value = CliqueSize(A,now_node);
+    G = build_adjacency_matrix(V,E);
+    
+    value = evaluation(now_node,G);
     now_value = value;
 
     for i = 1:N
@@ -31,27 +24,34 @@ function [value, nodes] = TS(V, E)
             end
         end
 
-        %1入れ替え
-        swap_list = Swap(A,now_node,tabu_list,V);
-        neighbor_list = vertcat(neighbor_list,swap_list);
-        %2追加
-        insert_list = Insert(A,now_node,tabu_list,V);
+        %1入れ替�?
+        %swap_list = Swap(A,now_node,tabu_list,V);
+        %neighbor_list = vertcat(neighbor_list,swap_list);
+        %2追�?
+        insert_list = Insert(G,now_node,tabu_list,V);
+        disp(insert_list);
         neighbor_list = vertcat(neighbor_list,insert_list);
         %3削除
-        delete_list = Delete(A,now_node,tabu_list,V);
+        delete_list = Delete(G,now_node,tabu_list,V);
+        disp(delete_list);
         neighbor_list = vertcat(neighbor_list,delete_list);
         
-        neighbor_list(size(neighbor_list,1),V+1) = 0;
+        if isempty(neighbor_list) == 0
+            break
+        else
+            disp(size(neighbor_list,1));
+            neighbor_list(size(neighbor_list,1),V+1) = 0;
+        end
         
         %クリークサイズ
         for j = 1:size(neighbor_list,1)
-            clique_num = CliqueSize(A,neighbor_list(j,1:V));
+            clique_num = evaluation(neighbor_list(j,1:V),A);
 
-            %listのV+1にクリークサイズを追加
+            %listのV+1にクリークサイズを追�?
             neighbor_list(j,V+1) = clique_num;
         end
         
-        %次の状態の洗濯
+        %次の状態�?�洗濯
         sort_list = [];
         r1 = 0;
         for k = V:-1:0
@@ -73,10 +73,10 @@ function [value, nodes] = TS(V, E)
         %選択解がbestな時更新
         if value < next_value
             value = next_value;
-            nodes_old = next_node;
+            nodes = next_node;
         end
 
-        %タブリストの更新
+        %タブリスト�?�更新
         for l = 1:V
             if now_node(l) == next_node(l) + 1
                 tabu_list(l) = tabu_size;
@@ -87,13 +87,7 @@ function [value, nodes] = TS(V, E)
         now_node = next_node;
         now_value = next_value;
 
-    end
-
-    for i = 1:V
-        if nodes_old(i) == 1
-            nodes(end+1) = i;
-        end
-    end            
+    end       
     
 end
 
@@ -162,22 +156,26 @@ function swap_list = Swap(A,nodes,tabu_list,V)
 
 end
 
-function insert_list = Insert(A,nodes,tabu_list,V)
+function insert_list = Insert(G,nodes,tabu_list,V)
     
     insert_list = [];
     for i = 1:V
         if nodes(i) == 0 && tabu_list(i) == 0
             new_node = nodes;
             new_node(i) = 1;
-            insert_list = CombList(insert_list,new_node);
+        else 
+            continue
         end
+        %if is_clique(new_node,G) == true
+            insert_list = CombList(insert_list,new_node);
+        %end
     end
     
 
 
 end
 
-function delete_list = Delete(A,nodes,tabu_list,V)
+function delete_list = Delete(G,nodes,tabu_list,V)
 
     delete_list = [];
 
@@ -190,7 +188,9 @@ function delete_list = Delete(A,nodes,tabu_list,V)
         if sum(new_node) == 0 
             continue
         end
-        delete_list = CombList(delete_list,new_node);
+        %if is_clique(new_node,G) == true
+            delete_list = CombList(delete_list,new_node);
+        %end
     end
 end
 
--- main.m
+++ main.m
@@ -19,9 +19,17 @@ for testcase = 1:TESTCASE_NUM
     V = importdata(sprintf(NODEFILE, testcase-1));
     E = importdata(sprintf(EDGEFILE, testcase-1));
     
+    disp(V);
+    disp(E);
+    
     G = build_adjacency_list(V, E);
     
+    disp(G);
+    
     for cnt = 1:10
+        
+        %{
+        
         fprintf("(%d, %d)", testcase, cnt);
         disp("---SA---");
         tic;
@@ -33,18 +41,18 @@ for testcase = 1:TESTCASE_NUM
         result{testcase, 1, 2, 1} = result{testcase, 1, 2, 1} + t;
         result{testcase, 1, 2, 2} = max(result{testcase, 1, 2, 2}, t);
         result{testcase, 1, 2, 3} = min(result{testcase, 1, 2, 3}, t);
-        
-         % disp("---TS---");
-         % tic;
-         % [value, nodes] = TS(G)
-         % t = toc
-         % result{testcase, 2, 1, 1} = result{testcase, 2, 1, 1} + value; 
-         % result{testcase, 2, 1, 2} = max(result{testcase, 2, 1, 2}, value);
-         % result{testcase, 2, 1, 3} = min(result{testcase, 2, 1, 3}, value);
-         % result{testcase, 2, 2, 1} = result{testcase, 2, 2, 1} + t;
-         % result{testcase, 2, 2, 2} = max(result{testcase, 2, 2, 2}, t);
-         % result{testcase, 2, 2, 3} = min(result{testcase, 2, 2, 3}, t);
-         
+        %}
+          disp("---TS---");
+          tic;
+          [value, nodes] = TS(G,V,E)
+          t = toc
+          result{testcase, 2, 1, 1} = result{testcase, 2, 1, 1} + value; 
+          result{testcase, 2, 1, 2} = max(result{testcase, 2, 1, 2}, value);
+          result{testcase, 2, 1, 3} = min(result{testcase, 2, 1, 3}, value);
+          result{testcase, 2, 2, 1} = result{testcase, 2, 2, 1} + t;
+          result{testcase, 2, 2, 2} = max(result{testcase, 2, 2, 2}, t);
+          result{testcase, 2, 2, 3} = min(result{testcase, 2, 2, 3}, t);
+        %{
         disp("---ILS---");
         tic;
         [value, nodes] = ILS(G)
@@ -55,7 +63,7 @@ for testcase = 1:TESTCASE_NUM
         result{testcase, 3, 2, 1} = result{testcase, 3, 2, 1} + t;
         result{testcase, 3, 2, 2} = max(result{testcase, 3, 2, 2}, t);
         result{testcase, 3, 2, 3} = min(result{testcase, 3, 2, 3}, t);
-
+        %}
         % disp("---GLS---");
         % tic;
         % [value, nodes] = GLS(G)
