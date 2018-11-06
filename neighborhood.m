00c85784ca11466ca428549b988d989f959e3c37
Yohei Shimmyo <qwpmb554@gmail.com>
2018-11-06 16:07:02 +0900

    widen the neighborhood.
    neighborhood becomes wide repeating reversing process

--- neighborhood.m
+++ neighborhood.m
@@ -1,4 +1,9 @@
 function state = neighborhood(state_struct, problemData)
-    S = neighborhoods(state_struct, problemData);
-    state = S(randi(size(S, 2)),:);
+    % S = neighborhoods(state_struct, problemData);
+    % state = S(randi(size(S, 2)),:);
+    %for i = 1:floor(state_struct.temperature)+1
+    state = state_struct.x;
+    idx = randi(size(state, 2));
+    state(idx) = mod(state(idx)+1, 2);
+    %end
 end
