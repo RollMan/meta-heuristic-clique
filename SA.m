3c0eed88ef2d6c624bfc9a4e14d5d9e3d3617c86
Yohei Shimmyo <qwpmb554@gmail.com>
2018-11-06 16:06:25 +0900

    add SA options: max iterations, initial temperature, and plotting function

--- SA.m
+++ SA.m
@@ -1,10 +1,10 @@
 function [x, fval, exitFlag, output] = SA(G)
-    rng default
     function f = eval(state)
         f = -evaluation(state, G);
     end    
-    options = optimoptions(@simulannealbnd, 'Datatype', 'custom','AnnealingFcn', @neighborhood);
+    options = optimoptions(@simulannealbnd, 'Datatype', 'custom', 'MaxIterations', 1000000, 'AnnealingFcn', @neighborhood, 'InitialTemperature', 10, 'MaxStallIterations', (size(G, 1)*500)^2, 'PlotFcn','saplotbestf');
     state = zeros(1, size(G, 1));
+    % state = randi(2, 1, size(G, 1)) - 1;
     [x, fval, exitFlag, output] = simulannealbnd(@eval, state, [], [], options);
     
 end
